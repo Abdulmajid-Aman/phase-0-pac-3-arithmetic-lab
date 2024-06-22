7200424948618eb6d2d12fe366ab1e264d15e11e
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-06-22 15:21:19 +0300

    made changes

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+    return a + b
+}
+function subtract(a,b) {
+    return a-b
+}
+function multiply(a,b) {
+    return a * b
+}
+function divide(a,b) {
+    return a/b
+}
+
+function increment(a) {
+    return a += 1
+}
+
+function decrement(a) {
+    return a-=1
+}
+
+function makeInt(a) {
+    return parseInt(a.toString(),10)
+    return parseInt('sldkjflksjf', 10)
+    return parseInt('0x2328', 10)
+}
+
+function preserveDecimal(a) {
+    return parseFloat(2.222)
+    return parseFloat(false)
+}
+
